2da7fcabf891580d7dfc59b877a522ee8e0a5915
alexi-ae <alexi.acunaespino@gmail.com>
2024-10-30 16:14:29 -0500

    create listener kafka consumer

---
+++ kafka-consumer/src/main/java/com/alexiae/kafka/consumer/listeners/KafkaConsumerListener.java
@@ -0,0 +1,17 @@
+package com.alexiae.kafka.consumer.listeners;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConsumerListener {
+    private Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerListener.class);
+
+    @KafkaListener(topics = {"producerDev-Topic"}, groupId = "first-group-id")
+    public void listener(String message) {
+        LOGGER.info("Mensaje recibido, mensaje:{}", message);
+    }
+}
