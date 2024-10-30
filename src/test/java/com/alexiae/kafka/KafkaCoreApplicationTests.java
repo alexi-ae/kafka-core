59488ddfcb68700275443bb13a7cf7978878074d
alexi-ae <alexi.acunaespino@gmail.com>
2024-10-30 12:22:55 -0500

    project base kafka

---
+++ src/test/java/com/alexiae/kafka/KafkaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiae.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
